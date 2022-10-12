68749deb108c6a8188c1768429ac7199fdb09f79
hhzh <simon.ho07@qq.com>
2022-10-12 16:56:13 +0800

    second commit

--- src/main/java/org/example/git/TestGit.java
+++ src/main/java/org/example/git/TestGit.java
@@ -9,5 +9,6 @@ package org.example.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello, Git");
+        System.out.println("Hello, Git2");
     }
 }
