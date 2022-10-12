00c4bec211b89bb9023f35b0c0acfafaf90449cb
simonho07 <115506511+simonho07@users.noreply.github.com>
2022-10-12 23:10:25 +0800

    pull commit

--- src/main/java/org/example/git/TestGit.java
+++ src/main/java/org/example/git/TestGit.java
@@ -15,5 +15,6 @@ public class TestGit {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test from idea");
+        System.out.println("pull test from github");
     }
 }
