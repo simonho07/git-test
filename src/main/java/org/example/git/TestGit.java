d49dc257101b9a5b7c718fa3ef7ebb9a8388d36e
hhzh <simon.ho07@qq.com>
2022-10-12 16:50:36 +0800

    first commit

---
+++ src/main/java/org/example/git/TestGit.java
@@ -0,0 +1,13 @@
+package org.example.git;
+
+/**
+ * @description: TODO 类描述
+ * @author: hhzh
+ * @e-mail: 1184057779@qq.com
+ * @date: 2022/10/12
+ **/
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("Hello, Git");
+    }
+}
